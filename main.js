8738ea6a0cfddf269ec8677a0ffcdabe6de08906
danielgarcia1c <110104441+danielgarcia1c@users.noreply.github.com>
2024-05-16 08:00:48 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,2 +1,9 @@
 const botoes = document.querySelectorAll(".botao");
 console.log(botoes);
+for (let i = 0; i < botoes.length ; i++){
+    botoes[i].onclick = function() {
+        for (j=0;j<botoes.length;j++){
+            botoes[j];this.classList.remove("ativo");
+        }
+    }
+}
