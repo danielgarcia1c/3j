988226761e604fe9189f71b81ce30977d8e44ea1
danielgarcia1c <110104441+danielgarcia1c@users.noreply.github.com>
2024-06-14 09:43:18 -0300

    14/06

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
 const botoes = document.querySelectorAll(".botao");
 const conteudo = document.querySelectorAll(".aba-conteudo")
 const tempo = document.querySelectorAll(".contador");
-const dataDeHoje = new Date();
+tempo[0].textContent="Ola"
+const tempoObjetivo1 = new Date(2024,8,4);
+const tempoObjetivo2 = new Date(2024,8,8);
+const tempoObjetivo3 = new Date(2025,2,30);
+const tempoObjetivo4 = new Date(2025,0,4);
+const agora = new Date()
 console
 console.log(tempo);
 for(let i = 0; i < botoes.length ; i++){
