6593cf8c45f12e2bbad6461e87c54574415dabbf
danielgarcia1c <110104441+danielgarcia1c@users.noreply.github.com>
2024-06-20 07:54:03 -0300

    20/06

--- main.js
+++ main.js
@@ -5,9 +5,17 @@ tempo[0].textContent="Ola"
 const tempoObjetivo1 = new Date(2024,8,4);
 const tempoObjetivo2 = new Date(2024,8,8);
 const tempoObjetivo3 = new Date(2025,2,30);
-const tempoObjetivo4 = new Date(2025,0,4);
+const tempoObjetivo4 = new Date(2025,4,4);
+let segundos;
+let minutos;
+let horas;
+let dias;
 const agora = new Date()
-console
+segundos = (tempoObjetivo1-agora)/1000
+minutos = segundos/60
+horas = minutos/60
+dias = horas/24
+tempo[0].textContent = tempoObjetivo1-agora
 console.log(tempo);
 for(let i = 0; i < botoes.length ; i++){
     botoes[i].onclick = function(){
